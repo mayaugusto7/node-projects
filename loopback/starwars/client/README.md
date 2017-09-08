ab3dd5b6478bbee8b7b51835a3f36c6d9a1aae48
Ilion Fabrício Nogueira <fabricionogueira10@gmail.com>
2017-09-08 11:03:43 -0300

    firs loopback

---
+++ loopback/starwars/client/README.md
@@ -0,0 +1,6 @@
+## Client
+
+This is the place for your application front-end files.
+
+node .
+run server
---
+++ loopback/starwars/server/boot/create-lb-tables.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var server = require('./../server');
+var ds = server.dataSources.postgresDs;
+var lbTables = ['User',
+                'AccessToken',
+                'ACL',
+                'RoleMapping',
+                'Role',
+                'People',
+                'Films',
+                'Starship',
+                'Species',
+                'Planet'
+];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
